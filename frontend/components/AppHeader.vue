e3e2db84da378702ade3cbccc1c57051037bd5f5
Rizky Faturriza <rfaturriza@gmail.com>
2025-02-18 21:01:23 +0700

    Refactor AppHeader component to dynamically render navigation links and improve current route highlighting

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -58,26 +58,17 @@
         <ul
           class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
-          <li>
+          <li v-for="(route, path) in routes" :key="path">
             <NuxtLink
-              to="/"
-              class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-700 md:p-0 md:dark:hover:text-primary-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              aria-current="page"
-              >Home</NuxtLink
-            >
-          </li>
-          <li>
-            <NuxtLink
-              to="/appointments"
-              class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-700 md:p-0 md:dark:hover:text-primary-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >Appointment</NuxtLink
-            >
-          </li>
-          <li>
-            <NuxtLink
-              to="/profile"
-              class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-700 md:p-0 md:dark:hover:text-primary-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >Profile</NuxtLink
+              :to="path"
+              :class="[
+                'block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-700 md:p-0 md:dark:hover:text-primary-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700',
+                currentRoute === path
+                  ? 'text-primary-700 underline dark:text-primary-500'
+                  : 'text-gray-900',
+              ]"
+              :aria-current="currentRoute === path ? 'page' : null"
+              >{{ route }}</NuxtLink
             >
           </li>
         </ul>
@@ -89,10 +80,15 @@
 <script setup>
 const router = useRouter();
 const { isAuthenticated } = useAuth();
-const currentPage = ref(router.currentRoute.value.path);
+const currentRoute = ref(router.currentRoute.value.path);
+const routes = {
+  "/": "Home",
+  "/appointments": "Appointment",
+  "/profile": "Profile",
+};
 
 watchEffect(() => {
-  currentPage.value = router.currentRoute.value.path;
+  currentRoute.value = router.currentRoute.value.path;
 });
 
 const logout = () => {
